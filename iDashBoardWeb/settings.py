3c43c046276796ab7f1471827af9c47f69786ac1
YunZhang <thuzyun@gmail.com>
2016-04-15 12:35:33 +0800

    Fake password

--- iDashBoardWeb/settings.py
+++ iDashBoardWeb/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'iDashBoard',
         'USER': 'root',
-        'PASSWORD': 'wuzher1||',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': 3306
     }
